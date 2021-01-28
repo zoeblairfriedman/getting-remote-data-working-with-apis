ced7c265e16cd711df9c216465aed46ea5f011bc
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-28 08:57:16 -0500

    commit tree 8307e782f2b1a2dd6e146432a5290cfa8c659a5b

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -12,7 +12,18 @@ class GetPrograms
     response.body
   end
 
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_programs)
+      programs.collect do |program|
+        program["agency"]  
+      end
+    end
+
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+# programs = GetPrograms.new.get_programs
+# puts programs
+
+programs = GetPrograms.new
+puts programs.program_school.uniq
